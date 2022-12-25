542d289f0b5ce302491c6aa7baa84a6841bde954
Hasan Emre AVARİSLİ <81103889+emreavarisli@users.noreply.github.com>
2022-12-25 16:37:12 +0300

    button

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,90 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: const HomeApp(),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Calculator App'),
-        ),
-        body: const MyHomePage(),
-      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class HomeApp extends StatefulWidget {
+  const HomeApp({super.key});
 
   @override
+  State<HomeApp> createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator App'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                alignment: Alignment.bottomRight,
+                child: const Text(
+                  '0',
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                customOutlineButton('9'),
+                customOutlineButton('8'),
+                customOutlineButton('7'),
+                customOutlineButton('+'),
+              ],
+            ),
+            Row(
+              children: [
+                customOutlineButton('4'),
+                customOutlineButton('5'),
+                customOutlineButton('6'),
+                customOutlineButton('-'),
+              ],
+            ),
+            Row(
+              children: [
+                customOutlineButton('1'),
+                customOutlineButton('2'),
+                customOutlineButton('3'),
+                customOutlineButton('x'),
+              ],
+            ),
+            Row(
+              children: [
+                customOutlineButton('C'),
+                customOutlineButton('0'),
+                customOutlineButton('='),
+                customOutlineButton('/'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget customOutlineButton(String value) {
+    return
+        //padding: const EdgeInsets.all(25),
+        TextButton(
+      onPressed: () {},
+      child: Text(
+        value,
+        style: const TextStyle(fontSize: 30.0),
+      ),
+    );
   }
 }
